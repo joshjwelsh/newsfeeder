bf3073fc8d674874b347628507f283c19af7519b
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-06-09 13:21:43 -0400

    fixed bug with interfaces

--- httpd/handler/newsfeed_get.go
+++ httpd/handler/newsfeed_get.go
@@ -7,7 +7,7 @@ import (
 	"github.com/joshjwelsh/newsfeeder/platform/newsfeed"
 )
 
-func NewsfeedGet(feed *newsfeed.Adder) gin.HandlerFunc {
+func NewsfeedGet(feed newsfeed.Getter) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		results := feed.GetAll()
 		c.JSON(http.StatusOK, results)
